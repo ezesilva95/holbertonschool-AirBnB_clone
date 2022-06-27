d00ab5d729c8e656beb38aeea277180bd0d62721
AxelBouvierM <axelbouvierm@gmail.com>
2022-06-27 15:03:42 +0000

    models

---
+++ models/base_model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from uuid import uuid4
+from datetime import datetime
+class BaseModel():
+    def __init__(self):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+    
+    def __str__(self):
+        string = "[" + __class__.__name__ + "]" + " (" + self.id + ") "
+        return ("{} {}".format(string, self.__dict__))
+
+    def save(self):
+        self.updated_at = datetime.now()
+    
+    def to_dict(self):
+        for elements in self.__dict__:
+            if elements == "created_at":
+                self.__dict__[elements] = str(self.__dict__[elements].isoformat())
+            elif elements == "updated_at":
+                self.__dict__[elements] = str(self.__dict__[elements].isoformat())
+        self.__dict__["__class__"] = __class__.__name__
+        return self.__dict__
