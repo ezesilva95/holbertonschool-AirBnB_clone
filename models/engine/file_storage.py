b00f32870b48532af34c8201d815731d56b949f5
AxelBouvierM <axelbouvierm@gmail.com>
2022-06-28 14:58:48 +0000

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,10 @@
 import json
 from logging import exception
 import os
+
+from importlib_metadata import files
 import models
+from models.base_model import BaseModel
 
 class FileStorage():
     __file_path = "file.json"
@@ -18,10 +21,12 @@ class FileStorage():
         tmp = {}
         for key in FileStorage.__objects.keys():
             tmp[key] = FileStorage.__objects[key].to_dict()
-        with open(FileStorage.__file_path, 'a+', encoding='utf-8') as f:
+        with open(FileStorage.__file_path, 'w+', encoding='utf-8') as f:
             json.dump(tmp, f)
     
     def reload(self):
         if os.path.isfile(FileStorage.__file_path) is True:
             with open(FileStorage.__file_path, 'r+', encoding='utf-8') as f:
                 obj = json.load(f)
+                for key, value in obj.items():
+                    FileStorage.__objects[key] = eval(value['__class__'])(**value)
