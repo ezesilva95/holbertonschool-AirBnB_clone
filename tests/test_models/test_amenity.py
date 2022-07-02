c264a91bcfd2665485f0e037b6148c9a7e46e2fc
ezesilva95 <4381@holbertonschool.com>
2022-07-02 07:59:53 -0700

    Test

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,15 @@
+""" testing for Amenity """
+import unittest
+import pep8
+from models.amenity import Amenity
+
+class Amenity_testing(unittest.TestCase):
+    """ check BaseModel """
+
+    def testpep8(self):
+        """ testing codestyle for Amenity """
+        pepstylecode = pep8.StyleGuide(quiet=True)
+        path_user = 'models/amenity.py'
+        result = pepstylecode.check_files([path_user])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
