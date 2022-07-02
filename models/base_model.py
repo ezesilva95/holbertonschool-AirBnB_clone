5a81ae864dc6a3438720ffef6c9697c878ee2073
ezesilva95 <4381@holbertonschool.com>
2022-07-02 02:07:25 -0700

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -4,22 +4,24 @@ from datetime import datetime, date
 import models
 import json
 import os
+
+
 class BaseModel():
     def __init__(self, *args, **kwargs):
         if kwargs is not None and len(kwargs) != 0:
-            for element in kwargs:
-                if element != "__class__":
-                    if element == "created_at":
-                        kwargs[element] = datetime.fromisoformat(kwargs[element])
+            for elemnt in kwargs:
+                if elemnt != "__class__":
+                    if elemnt == "created_at":
+                        kwargs[elemnt] = datetime.fromisoformat(kwargs[elemnt])
                     elif element == "updated_at":
-                        kwargs[element] = datetime.fromisoformat(kwargs[element])
-                    setattr(self, element, kwargs[element])
+                        kwargs[elemnt] = datetime.fromisoformat(kwargs[elemnt])
+                    setattr(self, elemnt, kwargs[element])
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
-    
+
     def __str__(self):
         string = "[" + type(self).__name__ + "]" + " (" + self.id + ") "
         return ("{} {}".format(string, self.__dict__))
@@ -28,7 +30,6 @@ class BaseModel():
         self.updated_at = datetime.now()
         models.storage.save()
 
-    
     def to_dict(self):
         dic = self.__dict__.copy()
         for elements in dic:
