f9758e80f0cb7e61f56374217c84fd11a48a803f
AxelBouvierM <axelbouvierm@gmail.com>
2022-07-02 11:12:09 +0000

    update comments

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+class BaseModel that defines all common attributes/methods for other classes
+"""
 from uuid import uuid4
 from datetime import datetime, date
 import models
@@ -7,7 +10,16 @@ import os
 
 
 class BaseModel():
+    """
+    Define a BaseModel class
+    """
     def __init__(self, *args, **kwargs):
+        """
+        Initialization the attributes of BaseModel
+        Args:
+            *args: Arguments passed
+            **kwargs: Arguments passed with key
+        """
         if kwargs is not None and len(kwargs) != 0:
             for elemnt in kwargs:
                 if elemnt != "__class__":
@@ -23,14 +35,23 @@ class BaseModel():
             models.storage.new(self)
 
     def __str__(self):
+        """
+        should print: [<class name>] (<self.id>) <self.__dict__>
+        """
         string = "[" + type(self).__name__ + "]" + " (" + self.id + ") "
         return ("{} {}".format(string, self.__dict__))
 
     def save(self):
+        """
+        updates updated_at with the current datetime
+        """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of __dict__
+        """
         dic = self.__dict__.copy()
         for elements in dic:
             if elements == "created_at":
