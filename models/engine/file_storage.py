5a901654ab32501a541def024c84b70ec66356af
ezesilva95 <4381@holbertonschool.com>
2022-07-02 02:16:16 -0700

    checker

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,13 +11,14 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
+
 class FileStorage():
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         return FileStorage.__objects
-    
+
     def new(self, obj):
         FileStorage.__objects[obj.__class__.__name__ + "." + obj.id] = obj
 
@@ -27,10 +28,10 @@ class FileStorage():
             tmp[key] = FileStorage.__objects[key].to_dict()
         with open(FileStorage.__file_path, 'w+', encoding='utf-8') as f:
             json.dump(tmp, f)
-    
+
     def reload(self):
         if os.path.isfile(FileStorage.__file_path) is True:
             with open(FileStorage.__file_path, 'r+', encoding='utf-8') as f:
                 obj = json.load(f)
-                for key, value in obj.items():
-                    FileStorage.__objects[key] = eval(value['__class__'])(**value)
+                for key, val in obj.items():
+                    FileStorage.__objects[key] = eval(val['__class__'])(**val)
